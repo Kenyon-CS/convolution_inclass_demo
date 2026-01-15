41a37305ecd04a9fab9089d8e12efcfd1a4fd0ef
James Skön <skonjp@kenyon.edu>
2026-01-15 17:02:07 -0500

    remove dir

--- src/#pragma once  #include <vector>  /convolve_fft.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-
-// FFT-based overlap-add convolution
-std::vector<float> convolve_fft_ola(const std::vector<float>& x,
-                                    const std::vector<float>& h,
-                                    size_t blockSize);
